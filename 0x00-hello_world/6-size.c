b788c6073acbd2cb86b6f58c11b03fbf45750911
zola2311 <zola2311@gmail.com>
2022-03-10 10:59:38 -0800

    final mandatory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 		int a;
@@ -20,4 +20,3 @@ int main(void)
 											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 												return (0);
 }
-
