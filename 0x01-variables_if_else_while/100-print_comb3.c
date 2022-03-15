16e7407de3c021f6727e054666e57b70ea58d118
zola2311 <zola2311@gmail.com>
2022-03-14 18:54:07 -0700

    11 hourr

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,31 @@
-			 								      #include <stdio.h>
-
+#include <stdio.h>
 /**
- * main - entry point
- *
- * Description: Print all possible different combinations of two digits
- * Return: 0
- */
+ *  * main - represent Entry Point to the program
+ *   *
+ *    * Return: 0 to stop the program
+ *     */
 int main(void)
 {
 	int i;
 	int j;
 
-	for (i = 0; i < 10; ++i)
+	for (i = 10; i <= 19; i++)
 	{
-		for (j = i + 1; j < 10; ++j)
+		for (j = 10; j <= 19; j++)
 		{
-			putchar('0' + i);
-		       	putchar('0' + j);
-
-			if (i != 8 || j != 9)
-	   		{
-				putchar(',');
-				putchar(' ');
+			if ((j % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
-																	       
