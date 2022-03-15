5648080848d81ed00318029fc3b0ff21fb89b174
zola2311 <zola2311@gmail.com>
2022-03-14 18:43:32 -0700

    11 hour

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,26 +8,26 @@
  */
 int main(void)
 {
-  int i;
-  int j;
+	int i;
+	int j;
 
-  for (i = 0; i < 10; ++i)
-    {
-      for (j = i + 1; j < 10; ++j)
+	for (i = 0; i < 10; ++i)
 	{
-	  putchar('0' + i);
-	  putchar('0' + j);
+		for (j = i + 1; j < 10; ++j)
+		{
+			putchar('0' + i);
+		       	putchar('0' + j);
 
-	  if (i != 8 || j != 9)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
+			if (i != 8 || j != 9)
+	   		{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-    }
 
-  putchar('\n');
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
 																	       
