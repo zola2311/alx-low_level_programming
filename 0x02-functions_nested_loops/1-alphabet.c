84e1285362d2e27ff47037c515c302372db7c7d1
zola2311 <zola2311@gmail.com>
2022-03-15 19:24:41 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main-print_alphabet.
+*Description:Print the alphabet in lowercase.
+* Return: void.
+*/
+void print_alphabet(void)
+{
+char j;
+for (j='a'; j<='z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
