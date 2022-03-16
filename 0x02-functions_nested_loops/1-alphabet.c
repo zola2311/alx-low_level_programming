5c14d75dc032273be6cdb7f75353998ec6abdf46
zola2311 <zola2311@gmail.com>
2022-03-15 20:45:25 -0700

    final

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-*main - print_alphabet.
-*Description: function that prints
-*Description :alphabet in lowercase
-*Return: void.
+ *main - print_alphabet.
+ *Description:print alphabet in lowercase
+ *Return: void.
 */
 void print_alphabet(void)
 {
