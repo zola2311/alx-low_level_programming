8b872f66fc18f2e8fcc9f0970b95e943160e65d0
zola2311 <zola2311@gmail.com>
2022-03-15 22:13:25 -0700

    test

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-*main- _isalpha - Shows 1 if the input is c
-*Description: The character in ASCII code*
+*_isalpha - Shows 1 if the input is c
+*@c: The character in ASCII code*
 *Return: 1 for letters. Returns 0 otherwise
 */
 int _isalpha(int c)
