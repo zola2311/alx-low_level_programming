3b0ddeead752da282e21a2e484595794365534fb
zola2311 <zola2311@gmail.com>
2022-03-16 16:40:16 -0700

    final

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - fibonacc 
+ *Return : Void
+ */
+int main(void)
+{
+double a = 0;
+double b = 1;
+double sum = 0;
+double f;
+int i, pair;
+
+for (i = 0; i < 32; i++)
+{
+f = a + b;
+pair = f;
+
+if (pair % 2 == 0)
+sum = sum + f;
+a = b;
+b = f;
+}
+printf("%.f\n", sum);
+return (0);
+}
