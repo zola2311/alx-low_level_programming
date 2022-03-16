b75e00d4b9fdcef042e63e09255576dd1b7bbadc
zola2311 <zola2311@gmail.com>
2022-03-15 18:14:40 -0700

    second

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- entry point to program
+ *
+ * return:for stop
+*/
+int main(void)
+{
+char a;
+
+for(a = 'a'; a <= 'z' ; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
