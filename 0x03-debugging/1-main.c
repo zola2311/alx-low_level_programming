4ecbf97edaeaa5ff3d446567ea6759c5f72a9d1e
zola2311 <zola2311@gmail.com>
2022-03-17 02:32:06 -0700

    second task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: 0
+ * Return: zero
  */
 
 int main(void)
@@ -12,6 +12,7 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
+ 
 /**
  * while (i < 10)
  *  {
