7497d7eb9e094b5c7e4a0e378dd12434a8748cf2
zola2311 <zola2311@gmail.com>
2022-03-17 16:32:37 -0700

    task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- *_isupper - checkes upper case 
- *@c: character
- *Return :1 or 0
+ * _isupper - checkes upper case 
+ *@c: print int
+ *Return :give 1 or 0
  */
 
 int _isupper(int c);
 {
-if(c >= 'A' && c <= 'Z')
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
-
 }
+
 else
 {
 return (0);
 }
-}   
+
+}
