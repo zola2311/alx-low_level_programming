b5ad9258c09a8bde171cebda4d6e89ae6790395c
zola2311 <zola2311@gmail.com>
2022-03-17 11:30:46 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -14,4 +11,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
