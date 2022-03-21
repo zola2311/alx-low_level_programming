223cc088a9d2e8fbbbea1553d0c9bdb6e4736bca
zola2311 <zola2311@gmail.com>
2022-03-21 09:50:26 -0700

    task_1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * swao_int - swap 2 int pointers
+ * swap_int - swap 2 int pointers
  * @a:input1
  * @b:input2
  * @c:swap variable
  */
-void swap_int(int *a,int *b)
+void swap_int(int *a, int *b)
 {
 int c = *a;
 *a = *b;
