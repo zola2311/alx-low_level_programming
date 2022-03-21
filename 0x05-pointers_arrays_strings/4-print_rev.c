350133f0add31f8ec53973a854815b7a13aad1c3
zola2311 <zola2311@gmail.com>
2022-03-21 13:05:20 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,14 +4,11 @@
  * @s: the string to be printed
  */
 {  
-//     declare variable  
-	    	        int i, len, temp;  
-	    	            len = strlen(str1); // use strlen() to get the length of str string  
-	    	                  
-	    	                    // use for loop to iterate the string   
-	    	                          for (i = 0; i < len/2; i++)  
-	                                  {  
-	    	                                      // temp variable use to temporary hold the string  
+int i, len, temp;  
+len = strlen(str1);  
+for (i = 0; i < len/2; i++)  
+{  
+	    	                                     
 	    	                                              temp = str1[i];  
 	    	                                                      str1[i] = str1[len - i - 1];  
 	    
