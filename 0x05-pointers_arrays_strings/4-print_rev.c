2c90a0f8ecf1502d7471ed17c4c027ac7b074aa1
zola2311 <zola2311@gmail.com>
2022-03-21 13:18:21 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,20 +2,43 @@
 #include <string.h>
 /**
  * print_rev - revrse printing 
- * @s: the string to be printed
+ * @s: the string t
  */
-void print_rev(char *str1)
-{  
-int i, len, temp;  
-len = strlen(str1);  
-for (i = 0; i < len/2; i++)  
-{  
-	    	                                     
-	    	                                              temp = str1[i];  
-	    	                                                      str1[i] = str1[len - i - 1];  
-	    
-	                                                               str1[len - i - 1] = temp;  
-	    
-	                                                                   }  
-	    
-	      }  
+
+void print_rev(char *s)
+  
+{
+  
+  int longi = 0;
+  
+  int o;
+  
+
+  
+  while (*s != '\0')
+    
+    {
+      
+      longi++;
+      
+      s++;
+      
+    }
+  
+  s--;
+  
+  for (o = longi; o > 0; o--)
+    
+    {
+      
+      _putchar(*s);
+      
+      s--;
+      
+    }
+  
+
+  
+  _putchar('\n');
+  
+}
