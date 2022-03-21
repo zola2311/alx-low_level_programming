e79a773f60a6f680ba57d58a909f602927f90f4e
zola2311 <zola2311@gmail.com>
2022-03-21 06:37:57 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,22 +1,6 @@
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
-
-	n=402;
-	printf("n=%d\n",n);
-	reset_to_98(&n);
-	printf("n=%d\n",n);
-	return(0);
-}
 /**
  * reset_to_98 - fun that reset num
- *
+ * @n: variable
  * Return: Always zero
  */
 void reset_to_98(int *n);
