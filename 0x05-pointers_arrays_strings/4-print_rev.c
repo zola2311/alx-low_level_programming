bd1026f44aecee3f0add947d0ecdbad98fa3f248
zola2311 <zola2311@gmail.com>
2022-03-21 13:26:37 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,40 +5,21 @@
  * @s: the string t
  */
 
-void print_rev(char *s)
-  
+void print_rev(char *s)  
 {
+int longi = 0; 
+int o;
   
-  int longi = 0;
-  
-  int o;
-  
-
-  
-  while (*s != '\0')
-    
-    {
-      
-      longi++;
-      
-      s++;
-      
-    }
-  
-  s--;
-  
-  for (o = longi; o > 0; o--)
-    
-    {
-      
-      putchar(*s);
-      
-      s--;
-      
-    }
-  
-
-  
-  putchar('\n');
-  
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
+{
+putchar(*s);
+s--;
+}
+putchar('\n');
 }
