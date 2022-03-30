397e7a02ffb75afd2905f0eadb1da9ccd095dd5f
zola2311 <zola2311@gmail.com>
2022-03-30 14:34:33 -0700

    final task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,13 @@
  */
 int _strlen_recursion(char *s)
 {
-  int len = 0;
+int len = 0;
 
-  if (*s)
-    {
-      len++;
-      len += _strlen_recursion(s + 1);
-    }
+if (*s)
+{
+len++;
+len += _strlen_recursion(s + 1);
+}
 
-  return (len);
+return (len);
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,15 @@
  */
 int _pow_recursion(int x, int y)
 {
-  int result = x;
+int result = x;
 
-  if (y < 0)
-    return (-1);
+if (y < 0)
+return (-1);
 
-  else if (y == 0)
-    return (1);
+else if (y == 0)
+return (1);
 
-  result *= _pow_recursion(x, y - 1);
+result *= _pow_recursion(x, y - 1);
 
-  return (result);
+return (result);
 }
