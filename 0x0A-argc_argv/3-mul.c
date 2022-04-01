b8e666223d3ebfe3b6dbc27e6fb3bd4775282d58
zola2311 <zola2311@gmail.com>
2022-04-01 10:20:17 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
-  int num1, num2, prod;
+int num1, num2, prod;
 
-  if (argc != 3)
-    {
-      printf("Error\n");
-      return (1);
-    }
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 
-  num1 = atoi(argv[1]);
-  num2 = atoi(argv[2]);
-  prod = num1 * num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+prod = num1 * num2;
 
-  printf("%d\n", prod);
+printf("%d\n", prod);
 
-  return (0);
+return (0);
 }
