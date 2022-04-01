0c3f35ea61c2bfdb602f0e8df5e5c7ae474d7b98
zola2311 <zola2311@gmail.com>
2022-04-01 14:17:22 -0700

    0x09. C - Static libraries

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,106 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
+int _atoi(char *s);
+int multiply_two_nums(int num1, int num2);
+void print_number(int n);
 
 /**
- * main - Prints the multiplication of two numbers, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - Entry point
  *
- * Return: If the program receives two arguments - 0.
- *         If the program does not receive two arguments - 1.
+ * @argc: Length of @argv
+ *
+ * @argv: Array of strings of the arguments of this program
+ *
+ * Return: 0, Success
  */
+
 int main(int argc, char *argv[])
 {
-int num1, num2, prod;
-
-if (argc != 3)
+if (argc == 3)
 {
-printf("Error\n");
-return (1);
+print_number(multiply_two_nums(_atoi(argv[1]), _atoi(argv[2])));
+_putchar('\n');
+return (0);
+}
+else
+{
+_putchar('E');
+_putchar('r');
+_putchar('r');
+_putchar('o');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-prod = num1 * num2;
+/**
+ * _atoi - Convert a number in a string to an integer
+ *
+ * @s: String to be converted
+ *
+ * Return: Integer converted OR (0) if none
+ */
 
-printf("%d\n", prod);
+int _atoi(char *s)
+{
+int sign_cache = 1;
+int result = 0;
 
-return (0);
+while (s[0] != '\0')
+{
+if (s[0] == '-')
+sign_cache *= -1;
+else if (s[0] >= '0' && s[0] <= '9')
+result = (result * 10) + (s[0] - '0');
+else if (result)
+break;
+
+s++;
+}
+
+return (result * sign_cache);
+}
+
+/**
+ * multiply_two_nums - Multiplies two numbers
+ *
+ * @num1: A number
+ *
+ * @num2: A number
+ *
+ * Return: Result of num1 * num2
+ */
+
+int multiply_two_nums(int num1, int num2)
+{
+return (num1 * num2);
+}
+
+/**
+ * print_number - Prints numbers chars
+ *
+ * @n: Integers params
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+unsigned int n1;
+
+n1 = n;
+
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
+}
+
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+
+_putchar((n1 % 10) + '0');
 }
