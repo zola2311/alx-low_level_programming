99e10aac2bcf7dd0e8a4fd83ad220f7c9b4b8963
zola2311 <zola2311@gmail.com>
2022-04-07 13:44:13 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 /**
  * malloc_checked - Allocates memory using malloc.
  * @b: The number of bytes to be allocated.
- *
+ * *mem - returns m
  * Return: A pointer to the allocated memory.
  */
 void *malloc_checked(unsigned int b)
