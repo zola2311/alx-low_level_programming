4284c97e1feb8780d8631728fbcb548a810c89ce
zola2311 <zola2311@gmail.com>
2022-04-11 13:57:04 -0700

    all tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
+/**
  * File: dog.h
  * Auth: Brennan D Baraban
  * Desc: Header file that defines a new type struct dog.
@@ -19,4 +19,14 @@ char *name;
 float age;
 char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
