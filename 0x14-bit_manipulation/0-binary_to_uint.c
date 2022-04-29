15084d24a0b449254e1832ad5df6d45004c469ee
zola2311 <zola2311@gmail.com>
2022-04-28 18:06:26 -0700

    bit manupulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+unsigned int num = 0, mult = 1;
+int len;
 
-	if (*b == '\0')
-		return (0);
+if (*b == '\0')
+return (0);
 
-	for (len = 0; b[len];)
-		len++;
+for (len = 0; b[len];)
+len++;
 
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+for (len -= 1; len >= 0; len--)
+{
+if (b[len] != '0' && b[len] != '1')
+return (0);
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
-	}
+num += (b[len] - '0') * mult;
+mult *= 2;
+}
 
-	return (num);
+return (num);
 }
